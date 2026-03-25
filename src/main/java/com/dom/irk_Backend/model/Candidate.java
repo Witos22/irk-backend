c4ba8f71070e82c1dc722fd016467008f78d6f6e
Witold <34158@uniwersytetkaliski.edu.pl>
2026-03-25 17:51:02 +0100

    ..

--- src/main/java/com/dom/irk_Backend/model/Candidate.java
+++ src/main/java/com/dom/irk_Backend/model/Candidate.java
@@ -1,11 +1,15 @@
 package com.dom.irk_Backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "candidates")
 public class Candidate {
